a24f0010330dcb8092629d7ee1a695cff8c8e96b
ChongWeiJie29 <chongweijie29@gmail.com>
2023-08-09 16:56:37 +0800

    Created Start planning page

--- WebFrontEnd/app/components/input.tsx
+++ WebFrontEnd/app/components/input.tsx
@@ -3,12 +3,12 @@ import { useState } from "react";
 import { Text, TextInput, View } from "react-native";
 import tw from "twrnc";
 
-export default function Input({ parameter }) {
+export default function Input({ parameter, width }) {
   const [value, setValue] = useState("");
 
   return (
     <View style={tw`flex-row items-center mb-4`}>
-      <Text style={tw`text-base font-semibold w-55`}>{parameter}:</Text>
+      <Text style={tw`text-base font-semibold w-${width}`}>{parameter}:</Text>
       <TextInput value={value} onChangeText={setValue} style={tw`w-70 h-10 border border-black rounded p-2 focus:border-blue-500`}/>
     </View>
   );
--- WebFrontEnd/app/index.tsx
+++ WebFrontEnd/app/index.tsx
@@ -1,23 +1,13 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
 import tw from "twrnc";
-import Input from "./components/input";
-import Title from "./components/title";
-import Button from "./components/button";
+import RegistrationPage from "./pages/registrationPage";
+import StartPlanningPage from "./pages/startPlanningPage";
 
-export default function RegistrationPage() {
+export default function App() {
 
   return (
-    <View style={tw`flex items-center justify-center h-4/6 p-8`}>
-      <Title parameter="Create your account"/>
-      <Input parameter="Name" />
-      <Input parameter="Prefered trip starting time" />
-      <Input parameter="Prefered trip ending time" />
-      <Input parameter="Interested themes" />
-      <Button>
-        <Text style={tw.style("text-slate-900")}>Create your account!</Text>
-      </Button>
-    </View>
+      <RegistrationPage></RegistrationPage>
   );
 }
 
