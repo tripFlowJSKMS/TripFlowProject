d24157fbf8eedeb046694e6dbce7f8d4193b8730
ChongWeiJie29 <chongweijie29@gmail.com>
2023-08-09 15:48:56 +0800

    Made Web FE Registration page

---
+++ WebFrontEnd/babel.config.js
@@ -0,0 +1,21 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "@babel/plugin-proposal-export-namespace-from",
+      "react-native-reanimated/plugin",
+      require.resolve("expo-router/babel"),
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            "@": ".",
+          }
+        }
+      ]
+    ],
+  };
+};
