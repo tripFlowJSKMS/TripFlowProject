ed549b870c50f0200cf1eacc72c0349b1a3bcebf
ChongWeiJie29 <chongweijie29@gmail.com>
2023-09-04 22:22:30 +0800

    Fixed file casing in HomePage

--- WebFrontEnd/app/components/homePageComponents/HomePageTopBar.tsx
+++ WebFrontEnd/app/components/homePageComponents/HomePageTopBar.tsx
@@ -9,7 +9,7 @@ export default function HomePageTopBar() {
       <View style={tw`flex-row items-center justify-between p-6 bg-transparent`}>
         <Text style={tw`text-lg font-semibold m-2`}>TripFlow</Text>
           <View style={tw`flex-row items-center justify-centre`}>
-            <Link href="/pages/StartPlanningPage">
+            <Link href="/pages/startPlanningPage">
               <Text style={tw`text-5xl font-semibold mr-5 mb-3`}>+</Text>
             </Link>
             <TouchableOpacity style={tw`w-10 h-10 bg-blue-500 rounded-full flex items-center justify-center m-1`}>
