108b356340ac6d54c2de3e23e26b51300d166f8b
jinyang628 <81862152+jinyang628@users.noreply.github.com>
2023-12-19 19:38:48 +0800

    Date range picker done

--- WebFrontEnd/app/components/startPlanningComponents/DatePicker.tsx
+++ WebFrontEnd/app/components/startPlanningComponents/DatePicker.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import tw from "twrnc";
 import formatDate from "@/app/helpers/formatDate";
@@ -19,6 +19,8 @@ export default function DatePicker({ currentDate, startDate, endDate, setStartDa
   const currentDateDate: string = currentDate.substring(8,10); // 18
   const [selectedYear, setSelectedYear] = useState<number>(parseInt(currentDateYear)); // 2023
   const [selectedMonth, setSelectedMonth] = useState<number>(parseInt(currentDateMonth)); // 11 (but its actually December)
+  const [selectedYear2, setSelectedYear2] = useState<number>(selectedMonth === 11 ? (selectedYear + 1) : selectedYear);
+  const [selectedMonth2, setSelectedMonth2] = useState<number>(selectedMonth === 11 ? 0: (selectedMonth + 1));
   const [selectedDate, setSelectedDate] = useState<number>(parseInt(currentDateDate)); // 18
   const [localStartDate, setLocalStartDate] = useState(startDate); // 2023-11-18 (but its actually December)
   const [localEndDate, setLocalEndDate] = useState(endDate); // 2023-11-18 (but its actually December)
@@ -48,23 +50,12 @@ export default function DatePicker({ currentDate, startDate, endDate, setStartDa
     "December",
   ];
 
-  // Calculate the day of the week (0 = Sunday, 1 = Monday, etc.)
-  const getDayOfWeek = (year: number, month: number, date: number): number => {
-    return new Date(year, month - 1, date).getDay();
-  };
-
-  const generateDatesForMonth = (year: number, month: number): number[] => {
-    const daysInMonth = new Date(year, month, 0).getDate();
-    const firstDayOfWeek = getDayOfWeek(year, month, 1); // 0 = Sunday, 1 = Monday, etc.
-    const dates = Array.from({ length: daysInMonth }, (_, i) => i + 1);
-
-    // Add empty placeholders for days before the 1st day of the month
-    for (let i = 0; i < firstDayOfWeek; i++) {
-      dates.unshift(null);
-    }
-
-    return dates;
-  };
+  const generateDatesForMonth = (year: number, month: number): (number | string)[] => {
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    const firstDayOfWeek = new Date(year, month, 1).getDay();
+    return Array.from({ length: daysInMonth + firstDayOfWeek }, (_, i) => 
+    i >= firstDayOfWeek ? i - firstDayOfWeek + 1 : `placeholder-${i}`);
+  }
 
   const getDateFromNumber = (year: number, month: number, date: number): Date => {
     return new Date(year, month, date);
@@ -81,10 +72,10 @@ export default function DatePicker({ currentDate, startDate, endDate, setStartDa
     const selectedDateObject: Date = getDateFromNumber(year, month, date); // Ensure month is zero-indexed here
     const endDateObject: Date = getDateFromString(localEndDate.substring(0,4), localEndDate.substring(5,7), localEndDate.substring(8,10));
     const startDateObject: Date = getDateFromString(localStartDate.substring(0,4), localStartDate.substring(5,7), localStartDate.substring(8,10));
-    // console.log("Current date object: " + currentDateObject);
-    // console.log("Selected date object: " + selectedDateObject);
-    // console.log("Start date object: " + startDateObject);
-    // console.log("End date object: " + endDateObject);
+    console.log("Current date object: " + currentDateObject);
+    console.log("Selected date object: " + selectedDateObject);
+    console.log("Start date object: " + startDateObject);
+    console.log("End date object: " + endDateObject);
     if (selectedDateObject < currentDateObject) {
       console.log("guard clause activated");
       return;
@@ -107,30 +98,45 @@ export default function DatePicker({ currentDate, startDate, endDate, setStartDa
     }
   }
 
-  const handleYearChange = (year: number): void => {
-    setSelectedYear(year);
-    adjustStartEndDate(year, selectedMonth, selectedDate);
-    setIsPickingStart(!isPickingStart); // Correctly toggle the state
-  };
-
-  // const handleMonthChange = (month: number): void => {
-  //   setSelectedMonth(month);
-  //   adjustStartEndDate(selectedYear, month, selectedDate);
+  // const handleYearChange = (year: number): void => {
+  //   setSelectedYear(year);
+  //   adjustStartEndDate(year, selectedMonth, selectedDate);
   // };
 
-  const handleMonthChange = (month: number): void => {
-    setSelectedMonth(month);
-    adjustStartEndDate(selectedYear, month, selectedDate);
-    setIsPickingStart(!isPickingStart); // Correctly toggle the state
-  };
-  
+  const handleMonthChange = (change: number): void => {
+    let newMonth = selectedMonth + change;
+    let newYear = selectedYear;
+    let newMonth2 = selectedMonth2 + change;
+    let newYear2 = selectedYear2;
 
-  const handleDateChange = (date: number): void => {
+    if (newMonth > 11) {
+      newMonth = 0;
+      newYear++;
+    } else if (newMonth < 0) {
+      newMonth = 11;
+      newYear--;
+    }
+
+    if (newMonth2 > 11) {
+      newMonth2 = 0;
+      newYear2++;
+    } else if (newMonth2 < 0) {
+      newMonth2 = 11;
+      newYear2--;
+    }
+
+    setSelectedMonth(newMonth);
+    setSelectedYear(newYear);
+    setSelectedMonth2(newMonth2);
+    setSelectedYear2(newYear2);
+};
+
+  const handleDateChange = (year: number, month: number, date: number): void => {
     // console.log("handleDateChange");
     // console.log(date);
     // console.log(selectedMonth);
     setSelectedDate(date);
-    adjustStartEndDate(selectedYear, selectedMonth, date);
+    adjustStartEndDate(year, month, date);
     setIsPickingStart(!isPickingStart); // Correctly toggle the state
   };
 
@@ -144,75 +150,73 @@ export default function DatePicker({ currentDate, startDate, endDate, setStartDa
     return date >= startDate && date <= endDate;
   } 
 
+  const dates1 = generateDatesForMonth(selectedYear, selectedMonth);
+  const dates2 = generateDatesForMonth(selectedYear2, selectedMonth2);
+
   return (
     <View>
+      {/* Header and Month/Year Selection */}
       <Text style={tw`text-center text-xl font-bold w-full my-3`}>Travel Dates</Text>
-      <View>
-        <View style={styles.row}>
-          <TouchableOpacity style={styles.arrowButton} onPress={() => handleYearChange(selectedYear - 1)}>
-            <Text>{"<"}</Text>
-          </TouchableOpacity>
-            <Text style={styles.headerText}>{selectedYear}</Text>
-          <TouchableOpacity style={styles.arrowButton} onPress={() => handleYearChange(selectedYear + 1)}>
-            <Text>{">"}</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.row}>
-          <TouchableOpacity style={styles.arrowButton}
-            onPress={() => handleMonthChange(selectedMonth - 1 === -1 ? 11 : selectedMonth - 1)}>
-            <Text>{"<"}</Text>
-          </TouchableOpacity>
-            <Text style={styles.headerText}>
-              {monthNames[selectedMonth]}
-            </Text>
-          <TouchableOpacity style={styles.arrowButton} onPress={() => handleMonthChange(selectedMonth + 1 === 12 ? 0 : selectedMonth + 1)}>
-            <Text>{">"}</Text>
-          </TouchableOpacity>
-        </View>
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.arrowButton} onPress={() => handleMonthChange(-1)}>
+          <Text>{"<"}</Text>
+        </TouchableOpacity>
+        <Text style={styles.headerText}>{monthNames[selectedMonth]} {selectedYear}</Text>
+        <Text style={styles.headerText}>{monthNames[selectedMonth2]} {selectedYear2}</Text>
+        <TouchableOpacity style={styles.arrowButton} onPress={() => handleMonthChange(1)}>
+          <Text>{">"}</Text>
+        </TouchableOpacity>
+      </View>
 
-        {/* I know this is not centered and the proper way to do it is instantiate one header
-            and put the corresponding dates below in a grid format but i cant seem to get the logic out.
-            If anyone can do it please revise this portion of the code */}
-        <View>
-          <View style={tw`flex flex-row justify-around py-[3%] `}>
-              <Text style={tw`font-semibold text-gray-600 text-xs`}>Sun</Text>
-              <Text style={tw`font-semibold text-gray-600 text-xs`}>Mon</Text>
-              <Text style={tw`font-semibold text-gray-600 text-xs`}>Tue</Text>
-              <Text style={tw`font-semibold text-gray-600 text-xs`}>Wed</Text>
-              <Text style={tw`font-semibold text-gray-600 text-xs`}>Thu</Text>
-              <Text style={tw`font-semibold text-gray-600 text-xs`}>Fri</Text>
-              <Text style={tw`font-semibold text-gray-600 text-xs`}>Sat</Text>
-          </View>
+      {/* Day Headers */}
+      <View style={tw`flex flex-row justify-around py-[3%]`}>
+        {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map(day => (
+          <Text key={day} style={tw`font-semibold text-gray-600 text-xs`}>{day}</Text>
+        ))}
+      </View>
 
-          <View style={tw`flex flex-row flex-wrap`}>
-  {generateDatesForMonth(selectedYear, selectedMonth).map((date, index) => {
-    if (date === null) {
-      return <View key={`placeholder-${index}`} style={tw`w-1/7 p-2`}></View>;
-    }
-    
-    // This needs to be -1 so that it is referring to the correct button
-    // const dateObject: Date = new Date(selectedYear, selectedMonth - 1, date);
-    const dateObject: Date = new Date(selectedYear, selectedMonth, date); // selectedMonth is already zero-indexed
-    // console.log(dateObject);
-    // console.log("error reaches here");
-    // console.log(localStartDate);
-    // console.log(localEndDate);
-    const isInRange = isDateInRange(dateObject, localStartDate, localEndDate);
-    const buttonStyle = isInRange ? tw`bg-blue-500` : tw``;
+      {/* Dates for Selected Month */}
+      <View style={tw`flex flex-row flex-wrap`}>
+        {dates1.map((date, index) => {
+          if (typeof date === 'string') { // This is a placeholder
+            return <View key={date} style={tw`w-1/7 p-2`}></View>;
+          }
 
-    return (
-      <TouchableOpacity 
-        key={`${date}-${index}`}
-        onPress={() => handleDateChange(date)}
-        style={tw.style(`w-1/7 p-2 border border-gray-400 rounded-lg`, buttonStyle)}>
-        <Text style={tw`text-center text-base`}>{date}</Text>
-      </TouchableOpacity>
-    );
-  })}
-</View>
+          const dateObject = getDateFromNumber(selectedYear, selectedMonth, date);
+          const inRange = isDateInRange(dateObject, localStartDate, localEndDate);
+          const buttonStyle = inRange ? tw`bg-blue-500` : tw``;
+          
+          return (
+            <TouchableOpacity 
+              key={`${date}-${index}`} 
+              onPress={() => handleDateChange(selectedYear, selectedMonth, date)}
+              style={tw.style(`w-1/7 p-2 border border-gray-400 rounded-lg`, buttonStyle)}>
+              <Text style={tw`text-center text-base`}>{date}</Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
 
+      {/* Dates for Next Month */}
+      <View style={tw`flex flex-row flex-wrap`}>
+        {dates2.map((date, index) => {
+          if (typeof date === 'string') { // This is a placeholder
+            return <View key={date} style={tw`w-1/7 p-2`}></View>;
+          }
 
-        </View>
+          const dateObject = getDateFromNumber(selectedYear2, selectedMonth2, date);
+          const inRange = isDateInRange(dateObject, localStartDate, localEndDate);
+          const buttonStyle = inRange ? tw`bg-blue-500` : tw``;
+          
+          return (
+            <TouchableOpacity 
+              key={`${date}-${index}`} 
+              onPress={() => handleDateChange(selectedYear2, selectedMonth2, date)}
+              style={tw.style(`w-1/7 p-2 border border-gray-400 rounded-lg`, buttonStyle)}>
+              <Text style={tw`text-center text-base`}>{date}</Text>
+            </TouchableOpacity>
+          );
+        })}
       </View>
     </View>
   );
