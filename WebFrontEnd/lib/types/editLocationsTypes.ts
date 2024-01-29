02767b66b33445d9a979853448ebacfff9da81d5
jinyang628 <81862152+jinyang628@users.noreply.github.com>
2024-01-28 16:17:44 -0800

    Correct zod validation

--- WebFrontEnd/lib/types/editLocationsTypes.ts
+++ WebFrontEnd/lib/types/editLocationsTypes.ts
@@ -1,10 +1,14 @@
 import { z } from "zod";
 import { destinationType } from "./commonTypes";
 
-export const generatedItinerarySchema = z.array(z.object({
-    destination: destinationType,
-    startingTime: z.number(),
-    endingTime: z.number(),  
-  }));
-
+export const generatedItinerarySchema = z.object({
+  itinerary: z.array(
+    z.object({
+      destination: destinationType,
+      stringDate: z.string(),
+      startingTime: z.number(),
+      endingTime: z.number(),
+    }),
+  ),
+});
 export const tripFlowAlgorithmSchema = z.array(destinationType);
